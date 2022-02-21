33108a0772f5509dd1f28b892d7919c524513398
sunheal <sunheal@gmail.com>
2022-02-20 22:28:06 -0500

    Created basic structure for cow-list

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
